873d7d6e6c6c0e9a65a9151ab91d437b54efbb90
Krystal <kralvarez@student.apc.edu.ph>
2025-08-11 03:11:02 +0000

    Edited Task 06

---
+++ week/week04/LoopExercise.java
@@ -0,0 +1,23 @@
+// Program to print a text 5 times
+
+class Loop01 {
+  public static void main(String[] args) {
+
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    
+    // Firstnumber and press Enter
+    System.out.println("Firstnumber"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println(" Firstnumber " + Secondnumber);        
+  }
+}
+  
+    int n = 5;
+    // for loop  
+    for (int i = ; i <= n; ++i) {
+      System.out.println(" ");
+    }
+  }
+}
